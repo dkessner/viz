65a691362345ebff3ecc36fcdb5e132dc232fd10
Darren Kessner <dkessner@eitm.org>
2024-02-19 09:56:53 -0800

    added Scenes

--- viz.js
+++ viz.js
@@ -3,61 +3,103 @@
 //
 
 
-let mic;
+
+let scenes = [];
+let current = 0;
+
+let audioIn;
 
 
 function setup() {
-
   createCanvas(windowWidth, windowHeight);
 
-  // some browsers (e.g. Chrome) require user interaction before 
-  // allowing initialization of an audio context
-  getAudioContext().suspend();
+  scenes.push(new Scene_Black());
+  scenes.push(new Scene_AudioInfo());
+  scenes.push(new Scene2());
 }
 
 
-function initializeMicrophone() {
-  userStartAudio(); // starts audio context
-  mic = new p5.AudioIn();
-  mic.start();
+function initializeAudioIn() {
+  console.log("Initializing audio input.");
+  userStartAudio();
+  audioIn = new p5.AudioIn();
+  audioIn.start();
 }
 
 
 function draw() {
-  background(0);
+    scenes[current].display(audioIn);
 
-  fill(255);
-  text("audio context state: " + getAudioContext().state, 50, 50);
-
-  let vol = 0;
-
-  if (mic) {
-      vol = mic.getLevel(); // in [0,1]
-  }
-
-  let h = map(vol, 0, 1, 0, height/2);
-  fill(127);
-  stroke(255);
-  ellipse(width/2, height/2 - h, 50, 50);
+    fill(128);
+    noStroke();
+    textSize(32);
+    text("Scene: " + current + " " + scenes[current].name(), 50, height-50);
 }
 
 
 function keyPressed() {
+    if (!audioIn) initializeAudioIn();
     if (key === 'f') {
         // TODO: weird behavior (2 presses?)
         let fs = fullscreen();
         fullscreen(!fs);
     }
+
+    number = parseInt(key);
+    if (!isNaN(number) && number < scenes.length)
+        current = number;
 }
 
 
 function mousePressed() {
-    initializeMicrophone();
+    if (!audioIn) initializeAudioIn();
 }
 
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+}
+
+
+
+class Scene {
+    name() {return "Unknown";}
+    display() {}
+}
+
+
+class Scene_Black extends Scene {
+    name() {return "Black";}
+    display() {background(0);}
+}
+
+
+class Scene_AudioInfo {
+    name() {return "Audio Info";}
+    display(audioIn) {
+        background(0);
+        fill(255);
+        noStroke();
+        textSize(32);
+
+        let dy = 50;
+        let y = dy;
+        text("Audio context state: " + getAudioContext().state, 50, y+=dy);
+
+        let level = audioIn ? audioIn.getLevel() : 0;
+        text("Level: " + level.toFixed(2), 50, y+=dy);
+
+        let r = map(level, 0, 1, width/10, width/4);
+        fill(0, 0, 255);
+        stroke(255);
+        ellipse(width/2, height/2, r*2, r*2);
+    }
+}
+
+class Scene2 extends Scene {
+    display() {
+        background(0, 0, 255);
+    }
 }
 
 
